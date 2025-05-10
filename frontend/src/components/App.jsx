5f5f949a0625d0b4202acf46590f9ab8ad3f2db8
Egor Kurilko <egor.kurilko@gmail.com>
2025-05-10 12:21:23 +0300

    add protection for the main path

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { getIsAuthenticated } from '../store/slices/authSelectors';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
 import ChatPage from './pages/ChatPage';
@@ -12,20 +19,29 @@ import AuthNavbar from './AuthNavbar';
 import ModalsContainer from './modals/ModalsContainer';
 import { appRoutes } from '../routes';
 
-const App = () => (
-  <div className="d-flex flex-column h-100">
-    <BrowserRouter>
-      <AuthNavbar />
-      <ModalsContainer />
-      <Routes>
-        <Route path={appRoutes.main} element={<ChatPage />} />
-        <Route path={appRoutes.login} element={<LoginPage />} />
-        <Route path={appRoutes.signup} element={<SignupPage />} />
-        <Route path={appRoutes.notFound} element={<NotFoundPage />} />
-      </Routes>
-      <ToastContainer />
-    </BrowserRouter>
-  </div>
-);
+const App = () => {
+  const isAuthenticated = useSelector(getIsAuthenticated);
+  return (
+    <div className="d-flex flex-column h-100">
+      <BrowserRouter>
+        <AuthNavbar />
+        <ModalsContainer />
+        <Routes>
+          <Route
+            path={appRoutes.main}
+            element={
+              isAuthenticated ? <ChatPage /> : <Navigate to={appRoutes.login} replace />
+            }
+          />
+          {/* <Route path={appRoutes.main} element={<ChatPage />} /> */}
+          <Route path={appRoutes.login} element={<LoginPage />} />
+          <Route path={appRoutes.signup} element={<SignupPage />} />
+          <Route path={appRoutes.notFound} element={<NotFoundPage />} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
